3582e40f4224880314fd9fbd852c872928acf237
Eduardo Righes <eduardo.righes@gmail.com>
2025-11-15 21:24:40 +0000

    Training so far.

--- 01_09/b/src/main.rs
+++ 01_09/b/src/main.rs
@@ -1,4 +1,11 @@
+use std::fs;
+
 fn main() {
     let file_path = "my_file";
     let contents = "This is my new file! Congrats!!!".to_string();
+
+    match fs::write(file_path, &contents) {
+        Ok(()) => println!("contents written to {} successfully.", file_path),
+        Err(e) => println!("failed to write to {}: {}", file_path, e),
+    }
 }
--- 02_02/b/src/main.rs
+++ 02_02/b/src/main.rs
@@ -1,7 +1,18 @@
+use std::fs;
+
+fn print_wanted_lines_from_file(file_path: &str, wanted_string: &str) {
+    let contents = fs::read_to_string(file_path).unwrap();
+    for line in contents.lines() {
+        if line.contains(wanted_string) {
+            println!("found line: {}", line);
+        }
+    }
+}
+
 fn main() {
     let file_path = "file_with_lines";
     let wanted_string = "a";
 
     // Create the below function
-    //print_wanted_lines_from_file(file_path, wanted_string);
+    print_wanted_lines_from_file(file_path, wanted_string);
 }
