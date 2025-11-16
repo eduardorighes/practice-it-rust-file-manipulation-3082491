fbb58df9e5949371d02ed04afdc8308eb02bc214
Eduardo Righes <eduardo.righes@gmail.com>
2025-11-16 18:07:48 +0000

    Finished the Vector section

--- 03_11/b/src/main.rs
+++ 03_11/b/src/main.rs
@@ -1,8 +1,24 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn read_file(path: &str) -> Result<Vec<Vec<String>>, std::io::Error> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let mut result = Vec::new();
+
+    for line in reader.lines() {
+        let words: Vec<String> = line?
+            .split_whitespace()
+            .map(|word| word.to_string())
+            .collect();
+        result.push(words)
+    }
+    Ok(result)
+}
+
 fn main() {
     let file_path = "file_with_lines";
-
-    // Implement the read_file function
-    //let lines = read_file(&file_path).expect(&format!("Unable to read file <{}>", &file_path));
-
-    //println!("{:?}", lines);
+    let lines = read_file(&file_path).expect(&format!("Unable to read file <{}>", &file_path));
+    println!("{:?}", lines);
 }
--- 03_12/b/src/main.rs
+++ 03_12/b/src/main.rs
@@ -1,8 +1,27 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn read_file(path: &str) -> Result<Vec<Vec<String>>, std::io::Error> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let result: Vec<Vec<String>> = reader
+        .lines()
+        .filter_map(|line| {
+            if line.is_ok() {
+                Some(line.unwrap())
+            } else {
+                None
+            }
+        })
+        .map(|line| line.split_whitespace().map(|w| w.to_string()).collect())
+        .collect();
+
+    Ok(result)
+}
+
 fn main() {
     let file_path = "file_with_lines";
-
-    // Implement the read_file function
-    //let lines = read_file(&file_path).expect(&format!("Unable to read file <{}>", &file_path));
-
-    //println!("{:?}", lines);
+    let lines = read_file(&file_path).expect(&format!("Unable to read file <{}>", &file_path));
+    println!("{:?}", lines);
 }
